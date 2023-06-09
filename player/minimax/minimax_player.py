1dd78995d08709d07af3afdb8e910eb8a730f5c6
Artur Michalski <armic@eg.dk>
2023-06-09 20:52:58 +0200

    Minimax player fix

--- player/minimax/minimax_player.py
+++ player/minimax/minimax_player.py
@@ -29,42 +29,41 @@ class MinimaxPlayer(IPlayer):
 
     def get_and_make_next_move_for_player(self, game_state: IGameState) -> int:
         game_state_copy = game_state.clone()
-        evaluation_multiplier = 1 if game_state.is_first_player_move else 1
-        evaluation, move = self.minimax(game_state_copy, 0, True, None, evaluation_multiplier)
+        is_first_player = game_state.is_first_player_move
+        evaluation, move = self.minimax(game_state_copy, 0, True, None, is_first_player)
         game_state.make_move(move)
         return move
 
-    def minimax(self, game_state, current_depth, max_turn, move, evaluation_multiplier):
+    def minimax(self, game_state, current_depth, max_turn, move, is_first_player):
         if move is not None:
             game_state = game_state.clone()
             game_state.make_move(move)
         if current_depth == self.minimax_configuration.max_depth or game_state.game_status != GameStatus.InProgress:
-            return self.evaluate_state(game_state) * evaluation_multiplier, move
+            return self.evaluate_state(game_state, is_first_player), move
 
         if max_turn:
             max_evaluation = -math.inf, None
             for move in game_state.available_moves:
-                evaluation = self.minimax(game_state, current_depth+1, False, move, evaluation_multiplier)
-                if current_depth == 0:
-                    print(evaluation[0], move)
+                evaluation = self.minimax(game_state, current_depth+1, False, move, is_first_player)
                 if evaluation[0] > max_evaluation[0]:
                     max_evaluation = evaluation[0], move
             return max_evaluation
         else:
             min_evaluation = math.inf, None
             for move in game_state.available_moves:
-                evaluation = self.minimax(game_state, current_depth+1, True, move, evaluation_multiplier)
+                evaluation = self.minimax(game_state, current_depth+1, True, move, is_first_player)
                 if evaluation[0] < min_evaluation[0]:
                     min_evaluation = evaluation[0], move
             return min_evaluation
 
-    def evaluate_state(self, game_state):
+    def evaluate_state(self, game_state, is_first_player):
+        multiplier = 1 if is_first_player else -1
         match game_state.game_status:
             case GameStatus.InProgress:
-                return np.sum(game_state.board * self._heuristic_fields_values)
+                return multiplier * np.sum(game_state.board * self._heuristic_fields_values)
             case GameStatus.FirstPlayerWon:
-                return 10000 if game_state.is_first_player_move else -10000
+                return 10000 if is_first_player else -10000
             case GameStatus.SecondPlayerWon:
-                return -10000 if game_state.is_first_player_move else 10000
+                return -10000 if is_first_player else 10000
             case GameStatus.Draw:
                 return 0
