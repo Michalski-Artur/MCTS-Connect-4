2408630d7963180071c9ae83df39e6f785244632
Artur Michalski <armic@eg.dk>
2023-06-10 23:18:04 +0200

    Experiments fix

--- experiments.py
+++ experiments.py
@@ -6,12 +6,14 @@ from game_logic.game import Game
 from game_logic.game_config import GameConfig
 from game_logic.game_state import GameState
 from game_logic.game_status import GameStatus
+from player.history_heuristic.history_heuristic_configuration import HistoryHeuristicConfiguration
 
 from player.mcts.mcts_configuration import MctsConfiguration
 from player.mcts.mcts_player import MctsPlayer
 from player.ucb1_tuned.ucb1_tuned_player import Ucb1TunedPlayer
 from player.minimax.minimax_configuration import MinimaxConfiguration
 from player.minimax.minimax_player import MinimaxPlayer
+from player.history_heuristic.history_heuristic_player import HistoryHeuristicPlayer
 
 
 class Experiments:
@@ -24,7 +26,6 @@ class Experiments:
         game_state = GameState(self.game_config)
         game = Game(game_state, first_player, second_player, print_game_stats=False)
         game_status = game.play()
-
         score_mutex.acquire()
         if game_status == GameStatus.Draw:
             score['first_player']['score'] += 0.5
@@ -36,31 +37,34 @@ class Experiments:
         score_mutex.release()
 
     def play_in_threads(self, first_player, second_player):
-        score = {'first_player': {'player_type': first_player.get_name(), 'score': 0}, 
-                 'second_player': {'player_type': second_player.get_name(), 'score': 0}}
-        
+        score = {'first_player': {'player_type': first_player.player_name, 'score': 0},
+                 'second_player': {'player_type': second_player.player_name, 'score': 0}}
+
         score_mutex = threading.Lock()
         threads = []
 
         start_time = time.time()
         for _ in range(self.iterations):
-            thread = threading.Thread(target=self.play_single_game, args=(first_player, second_player, score_mutex, score))
+            thread = threading.Thread(target=self.play_single_game,
+                                      args=(first_player, second_player, score_mutex, score))
             thread.start()
             threads.append(thread)
-
         for thread in threads:
             thread.join()
-
         end_time = time.time()
         runtime = end_time - start_time
-        print(f"Runtime for {self.iterations} iterations is: {round(runtime/60, 4)} min")
-
+        print(f"Runtime for {self.iterations} iterations is: {round(runtime / 60, 4)} min")
         return score
 
     def run_experiments(self):
         mcts_config = MctsConfiguration(30_000, 10)
+        history_heuristic_config = HistoryHeuristicConfiguration(30_000, 10, 0.3)
         minimax_config = MinimaxConfiguration(6)
-        self.players = [MctsPlayer(mcts_config), Ucb1TunedPlayer(mcts_config), MinimaxPlayer(minimax_config)]
+        self.players = [
+            MctsPlayer(mcts_config),
+            Ucb1TunedPlayer(mcts_config),
+            MinimaxPlayer(minimax_config),
+            HistoryHeuristicPlayer(history_heuristic_config)]
 
         for player1, player2 in itertools.permutations(self.players, 2):
             self.global_score.append(self.play_in_threads(player1, player2))
