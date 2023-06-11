5f41dd81a2273524bf2b11c7ed5556fbc247fca1
Artur Michalski <armic@eg.dk>
2023-06-11 12:56:47 +0200

    Add check whether player move can lead to opponent win

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ from player.mcts.mcts_configuration import MctsConfiguration
 from player.mcts.mcts_player import MctsPlayer
 from player.minimax.minimax_configuration import MinimaxConfiguration
 from player.minimax.minimax_player import MinimaxPlayer
-from player.ucb1_tuned.ucb1_tuned_player import Ucb1TunedPlayer
 from player.score_bounded_mcts.score_bounded_mcts_player import ScoreBoundedMctsPlayer
+from player.ucb1_tuned.ucb1_tuned_player import Ucb1TunedPlayer
 
 
 def main():
@@ -20,7 +20,7 @@ def main():
 
     # MCTS
     mcts_configuration = MctsConfiguration(30_000, 10)
-    # first_player = MctsPlayer(mcts_configuration)
+    first_player = MctsPlayer(mcts_configuration)
     # second_player = MctsPlayer(mcts_configuration)
 
     # UCB1
@@ -34,13 +34,13 @@ def main():
     # second_player = HistoryHeuristicPlayer(history_heuristic_configuration)
 
     # Minimax
-    minimax_configuration = MinimaxConfiguration(6)  # 7 takes a while
+    minimax_configuration = MinimaxConfiguration(4)  # 7 takes a while
     # first_player = MinimaxPlayer(minimax_configuration)
     second_player = MinimaxPlayer(minimax_configuration)
 
-    #ScoreBoundedMcts
+    # ScoreBoundedMcts
     score_bounded_mcts_configuration = MctsConfiguration(30_000, 10)
-    first_player = ScoreBoundedMctsPlayer(mcts_configuration)
+    # first_player = ScoreBoundedMctsPlayer(mcts_configuration)
     # second_player = ScoreBoundedMctsPlayer(mcts_configuration)
 
     game_config = GameConfig()
